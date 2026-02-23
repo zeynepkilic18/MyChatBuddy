cb93572d709c002481598924cca959f80a13ef35
Zeynep Kılıç <18zeynepklc@gmail.com>
2026-02-23 01:17:52 -0800

    Initial Commit

---
+++ ChatBuddy/ChatBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBuddyApp.swift
+//  ChatBuddy
+//
+//  Created by Zeynep Kılıç on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChatBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
